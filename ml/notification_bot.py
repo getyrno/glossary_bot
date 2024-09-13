37ec66223f24f7c4c642a58e6d5753464e4b3b18
p.nikitin <p.nikitin@itorummr.com>
2024-09-13 03:46:18 +0300

    env

--- ml/notification_bot.py
+++ ml/notification_bot.py
@@ -1,9 +1,9 @@
 import os
-import requests
 import logging
+from telegram import Bot
 from dotenv import load_dotenv
 
-# Загрузка переменных окружения из файла .env
+# Загрузка переменных окружения
 load_dotenv()
 
 # Настройка логирования
@@ -13,32 +13,15 @@ logging.basicConfig(level=logging.INFO)
 TOKEN = os.getenv('TELEGRAM_BOT_TOKEN2')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-# Проверка значений
-logging.info(f"TOKEN: {TOKEN}")
-logging.info(f"CHAT_ID: {CHAT_ID}")
+# Инициализация бота
+bot = Bot(token=TOKEN)
 
 def send_message(text):
-    if not TOKEN or not CHAT_ID:
-        logging.error("Отсутствуют необходимые переменные окружения (TOKEN или CHAT_ID)")
-        return
-
-    url = f'https://api.telegram.org/bot{TOKEN}/sendMessage'
-    payload = {
-        'chat_id': CHAT_ID,
-        'text': text
-    }
-
-    response = None  # Инициализация переменной
-
     try:
-        response = requests.post(url, data=payload)
-        response.raise_for_status()
-        logging.info(f"Сообщение отправлено: {text}")
-    except requests.exceptions.RequestException as e:
+        bot.send_message(chat_id=CHAT_ID, text=text)
+        logging.info(f"Сообщение успешно отправлено: {text}")
+    except Exception as e:
         logging.error(f"Ошибка при отправке сообщения: {e}")
-        if response is not None:
-            logging.error(f"Ответ от Telegram: {response.text}")
-
 
 if __name__ == "__main__":
     send_message("Тестовое сообщение для проверки")
